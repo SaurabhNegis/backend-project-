9db2fdef96cfbfef8212a40a9b574294d8c2757d
Saurabh negi <negis9705@gmail.com>
2024-11-29 00:12:36 +0530

    addition some chages to aap.js index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
  dotenv.config({
     path: './env'
  })
 
 
-connectDB()
+connectDB() 
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Serber is running at PORT: ${process.env.PORT}`)
+   
+        app.on("error", (error) =>{
+        console.log("ERROR in App:", error);
+        throw error
+        })
+    })
+})
+.catch((error) => {
+    console.log("MONGO db connection Failed !!! ", error)
+})
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 import express from "express"
 const app = express()
