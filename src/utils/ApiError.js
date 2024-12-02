adf5fc7ea4610af5f6bf4a5683c63649ae82be41
Saurabh negi <negis9705@gmail.com>
2024-12-02 11:57:51 +0530

    all setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         massage="Something went-wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){         
         super(massage)
         this.statusCode = statusCode
@@ -13,7 +13,7 @@ class ApiError extends Error{
         this.errors = errors
 
         if (stack) {
-            this.stack = statck
+            this.stack = stack
 
         }else{
             Error.captureStackTrace(this, this.constructor);
